85407b05f99b8d39f72a9b4e878b864a7a998a47
Ismael <ismael.caraballo85@gmail.com>
2025-10-11 14:40:49 -0400

    homePage

--- GestDocSi2/settings.py
+++ GestDocSi2/settings.py
@@ -12,13 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import environ, os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", default="change-me")   # nunca dejes una real aquí
@@ -42,6 +40,7 @@ INSTALLED_APPS = [
     'actores',
     'documentos',
     'casos',
+    'portal',
 ]
 
 AUTH_USER_MODEL = 'seguridad.Usuario'
@@ -76,7 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "GestDocSi2.wsgi.application"
 ASGI_APPLICATION = "GestDocSi2.asgi.application"
 
-# === Base de datos: PostgreSQL desde .env ===
+# Base de datos: PostgreSQL desde .env 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -95,11 +94,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Archivos estáticos y media
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-#STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
