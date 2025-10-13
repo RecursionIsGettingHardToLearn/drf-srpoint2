d84bc24a88f56a1efd83634a1289adc7afe74e25
Tu Nombre <tuemail@example.com>
2025-10-13 19:47:35 -0400

    Mensaje descriptivo

--- chat/urls.py
+++ chat/urls.py
@@ -2,14 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConversacionViewSet, MensajeViewSet, ConsultaDocumentoViewSet, ConfiguracionIAViewSet
 
-# Crear el router y registrar los viewsets
-router = DefaultRouter()
-router.register(r'conversaciones', ConversacionViewSet)
-router.register(r'mensajes', MensajeViewSet)
-router.register(r'consultas_documentos', ConsultaDocumentoViewSet)
-router.register(r'configuraciones_ia', ConfiguracionIAViewSet)
 app_name = 'chat'
 
 urlpatterns = [
@@ -23,5 +16,4 @@ urlpatterns = [
     path('api/crear-conversacion/', views.crear_conversacion, name='crear_conversacion'),
     path('api/eliminar-conversacion/<int:conversacion_id>/', views.eliminar_conversacion, name='eliminar_conversacion'),
     path('api/sugerencias/', views.obtener_sugerencias, name='obtener_sugerencias'),
-    path('', include(router.urls)), 
 ]
