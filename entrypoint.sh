48bbb314aca1715d6eb1605246beca326793248a
Tu Nombre <tuemail@example.com>
2025-10-14 19:18:44 -0400

    s

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ python manage.py migrate --no-input
 # Create superuser (will only run once)
 python manage.py create_default_superuser
 
+mkdir -p /app/staticfiles
+chown -R appuser:appuser /app/staticfiles
+
 # Collect static files
 python manage.py collectstatic --no-input
 
